39db4b7d9a97e3277af8550406fdf640ed24a972
Colin Cross <ccross@google.com>
2024-12-02 12:38:49 -0800

    aoc01 part 1

---
+++ aoc01/src/main.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::fs::read_to_string;
+
+fn rotate_vector_of_vectors<T>(vec: &Vec<Vec<T>>) -> Vec<Vec<T>>
+where
+    T: Copy,
+{
+    assert!(!vec.is_empty());
+    assert!(!vec[0].is_empty());
+    (0..vec[0].len())
+        .map(|i| (0..vec.len()).map(|j| vec[j][i]).collect())
+        .collect()
+}
+
+fn list_distance(lines: &Vec<Vec<i64>>) -> i64 {
+    let mut lists = rotate_vector_of_vectors(lines);
+    lists.iter_mut().map(|x| x.sort()).last();
+    let pairs = rotate_vector_of_vectors(&lists);
+
+    pairs.iter().map(|x| (x[1] - x[0]).abs()).sum()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let lines = read_to_string(&args[1])
+        .unwrap()
+        .lines()
+        .map(str::split_whitespace)
+        .map(|x| x.map(|n| n.parse::<i64>().unwrap()).collect::<Vec<_>>())
+        .collect();
+
+    println!("list distance: {}", list_distance(&lines));
+}
