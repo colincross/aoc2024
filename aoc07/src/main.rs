81fc2534c5659daa4a0d94669255cfa3981a3897
Colin Cross <ccross@google.com>
2024-12-06 22:29:12 -0800

    aoc07 part 2

--- aoc07/src/main.rs
+++ aoc07/src/main.rs
@@ -40,32 +40,43 @@ impl Operator {
 
 const ADD: Operator = Operator { f: |a, b| a + b };
 const MUL: Operator = Operator { f: |a, b| a * b };
+const CONCAT: Operator = Operator {
+    f: |a, b| (10 as u64).pow(count_digits(b)) * a + b,
+};
 
-const OPERATORS: &[Operator] = &[ADD, MUL];
-const NUM_OPERATORS: u64 = OPERATORS.len() as u64;
+fn count_digits(n: u64) -> u32 {
+    (n as f64).log10() as u32 + 1
+}
 
-struct OperatorIterator {
+const FIRST_OPERATORS: &[Operator] = &[ADD, MUL];
+const SECOND_OPERATORS: &[Operator] = &[ADD, MUL, CONCAT];
+
+struct OperatorIterator<'a> {
+    operator_list: &'a [Operator],
     len: u32,
     i: u64,
 }
 
-impl OperatorIterator {
-    fn new(len: usize) -> Self {
+impl<'a> OperatorIterator<'a> {
+    fn new(operator_list: &'a [Operator], len: usize) -> Self {
         Self {
+            operator_list,
             len: len as u32,
             i: 0,
         }
     }
 }
 
-impl<'a> Iterator for OperatorIterator {
+impl<'a> Iterator for OperatorIterator<'a> {
     type Item = Vec<Operator>;
     fn next(&mut self) -> Option<Self::Item> {
-        if self.i < NUM_OPERATORS.pow(self.len) {
+        let num_operators = self.operator_list.len() as u64;
+        if self.i < num_operators.pow(self.len) {
             let mut operators = vec![ADD; self.len as usize];
             for (operator_number, operator) in operators.iter_mut().enumerate() {
-                *operator = OPERATORS[((self.i / NUM_OPERATORS.pow(operator_number as u32))
-                    % NUM_OPERATORS) as usize]
+                *operator = self.operator_list[((self.i
+                    / num_operators.pow(operator_number as u32))
+                    % num_operators) as usize]
             }
             self.i += 1;
             Some(operators)
@@ -75,15 +86,23 @@ impl<'a> Iterator for OperatorIterator {
     }
 }
 
-fn solvable_equation(equation: &Equation) -> bool {
-    OperatorIterator::new(equation.nums.len() - 1)
+fn solvable_equation(operators: &[Operator], equation: &Equation) -> bool {
+    OperatorIterator::new(operators, equation.nums.len() - 1)
         .any(|operators| equation.apply_operators(&operators) == equation.answer)
 }
 
 fn solvable_equations_sum(equations: &[Equation]) -> u64 {
     equations
         .iter()
-        .filter(|&equation| solvable_equation(equation))
+        .filter(|&equation| solvable_equation(FIRST_OPERATORS, equation))
+        .map(|equation| equation.answer)
+        .sum()
+}
+
+fn solvable_equations_with_concat_sum(equations: &[Equation]) -> u64 {
+    equations
+        .iter()
+        .filter(|&equation| solvable_equation(SECOND_OPERATORS, equation))
         .map(|equation| equation.answer)
         .sum()
 }
@@ -110,6 +129,10 @@ fn main() {
         "solvable equations sum: {}",
         solvable_equations_sum(&equations)
     );
+    println!(
+        "solvable equations with concat sum: {}",
+        solvable_equations_with_concat_sum(&equations)
+    );
 }
 
 #[cfg(test)]
@@ -130,5 +153,39 @@ mod tests {
         let equations = parse_input(&data);
         let solvable_equations_sum = solvable_equations_sum(&equations);
         assert_eq!(solvable_equations_sum, 8401132154762);
+    }
+
+    #[test]
+    fn test_part2() {
+        let data = read_to_string("src/test.txt").unwrap();
+        let equations = parse_input(&data);
+        let solvable_equations_with_concat_sum = solvable_equations_with_concat_sum(&equations);
+        assert_eq!(solvable_equations_with_concat_sum, 11387);
+    }
+
+    #[test]
+    fn answer_part2() {
+        let data = read_to_string("src/main.txt").unwrap();
+        let equations = parse_input(&data);
+        let solvable_equations_with_concat_sum = solvable_equations_with_concat_sum(&equations);
+        assert_eq!(solvable_equations_with_concat_sum, 95297119227552);
+    }
+
+    #[test]
+    fn test_concat() {
+        assert_eq!(CONCAT.apply(1, 2), 12);
+        assert_eq!(CONCAT.apply(15, 6), 156);
+        assert_eq!(CONCAT.apply(48, 6), 486);
+        assert_eq!(CONCAT.apply(17, 8), 178);
+        assert_eq!(CONCAT.apply(1000, 1000), 10001000);
+        assert_eq!(CONCAT.apply(12, 345), 12345);
+    }
+
+    #[test]
+    fn test_count_digits() {
+        assert_eq!(count_digits(1), 1);
+        assert_eq!(count_digits(5), 1);
+        assert_eq!(count_digits(10), 2);
+        assert_eq!(count_digits(11), 2);
     }
 }
