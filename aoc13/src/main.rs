f1b4285765b0cfd2f445555630c19b4e205902ba
Colin Cross <ccross@google.com>
2024-12-12 22:42:20 -0800

    aoc13 part 2

--- aoc13/src/main.rs
+++ aoc13/src/main.rs
@@ -1,24 +1,24 @@
 use regex::Regex;
 use std::fs::read_to_string;
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 struct Button {
-    cost: u32,
-    x: u32,
-    y: u32,
+    cost: u64,
+    x: u64,
+    y: u64,
 }
 
 impl Button {
-    const A_COST: u32 = 3;
-    const B_COST: u32 = 1;
+    const A_COST: u64 = 3;
+    const B_COST: u64 = 1;
 }
 
 #[derive(Debug)]
 struct Machine {
     a: Button,
     b: Button,
-    x: u32,
-    y: u32,
+    x: u64,
+    y: u64,
 }
 
 impl Machine {
@@ -47,36 +47,66 @@ Prize: X=(?<px>[0-9]+), Y=(?<py>[0-9]+)
             })
             .collect()
     }
+
+    fn add_10_trillion(&self) -> Self {
+        const INC: u64 = 10000000000000;
+        Self {
+            a: self.a,
+            b: self.b,
+            x: self.x + INC,
+            y: self.y + INC,
+        }
+    }
+
+    fn solve(&self) -> (u64, u64) {
+        // a * Ax + b * Bx = X
+        // a * Ay + b * By = Y
+        // a = (X - b * Bx) / Ax
+        // (X - b * Bx) / Ax * Ay + b * By = Y
+        // X * Ay / Ax - b * Bx * Ay / Ax + b * By = Y
+        // b * (By - Bx * Ay / Ax) = Y - X * Ay / Ax
+        // b = (Y - X * Ay / Ax) / (By - Bx * Ay / Ax)
+        let ax = self.a.x as f64;
+        let ay = self.a.y as f64;
+        let bx = self.b.x as f64;
+        let by = self.b.y as f64;
+        let x = self.x as f64;
+        let y = self.y as f64;
+
+        let b = (y - x * ay / ax) / (by - bx * ay / ax);
+        let a = (x - b * bx) / ax;
+
+        (a.round() as u64, b.round() as u64)
+    }
 }
 
 fn parse_input(data: &str) -> Vec<Machine> {
     Machine::from(data)
 }
 
-fn min_tokens(machine: &Machine) -> Option<u32> {
-    (0..100)
-        .filter_map(|a| {
-            if let Some(b) =
-                (machine.x.checked_sub(a * machine.a.x)).and_then(|f| Some(f / machine.b.x))
-            {
-                Some((a, b))
-            } else {
-                None
-            }
-        })
-        .filter(|&(a, b)| {
-            machine.a.x * a + machine.b.x * b == machine.x
-                && machine.a.y * a + machine.b.y * b == machine.y
-        })
-        .map(|(a, b)| a * machine.a.cost + b * machine.b.cost)
-        .min()
+fn min_tokens(machine: &Machine) -> Option<u64> {
+    let (a, b) = machine.solve();
+    if a * machine.a.x + b * machine.b.x == machine.x
+        && a * machine.a.y + b * machine.b.y == machine.y
+    {
+        Some(a * machine.a.cost + b * machine.b.cost)
+    } else {
+        None
+    }
 }
 
-fn min_total_tokens(machines: &[Machine]) -> u32 {
+fn min_total_tokens(machines: &[Machine]) -> u64 {
     machines
         .iter()
         .map(|machine| min_tokens(machine).unwrap_or_default())
         .sum()
+}
+
+fn add_10_trillion(machines: &[Machine]) -> Vec<Machine> {
+    machines
+        .iter()
+        .map(|machine| machine.add_10_trillion())
+        .collect()
 }
 
 fn main() {
@@ -93,6 +123,8 @@ fn main() {
     };
     let data = read_to_string(&input_file).unwrap();
     let machines = parse_input(&data);
+    println!("min total tokens: {}", min_total_tokens(&machines));
+    let machines = add_10_trillion(&machines);
     println!("min total tokens: {}", min_total_tokens(&machines));
 }
 
@@ -112,5 +144,12 @@ mod tests {
         let data = read_to_string("src/main.txt").unwrap();
         let machines = parse_input(&data);
         assert_eq!(min_total_tokens(&machines), 29877);
+    }
+
+    #[test]
+    fn answer_part2() {
+        let data = read_to_string("src/main.txt").unwrap();
+        let machines = add_10_trillion(&parse_input(&data));
+        assert_eq!(min_total_tokens(&machines), 99423413811305);
     }
 }
