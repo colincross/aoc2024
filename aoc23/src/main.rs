46001061e4682dfdb443425dfe03254edec45698
Colin Cross <ccross@google.com>
2024-12-24 18:32:03 -0800

    aoc23 part 2

--- aoc23/src/main.rs
+++ aoc23/src/main.rs
@@ -35,6 +35,56 @@ fn count_interconnected_computers_with_t(connections: &Connections) -> usize {
     connected.len()
 }
 
+fn bron_kerbosch(
+    connections: &Connections,
+    r: &HashSet<String>,
+    p: &HashSet<String>,
+    x: &HashSet<String>,
+    maximal_cliques: &mut HashSet<Vec<String>>,
+) {
+    if p.is_empty() && x.is_empty() {
+        let mut clique = r.iter().cloned().collect::<Vec<_>>();
+        clique.sort();
+        maximal_cliques.insert(clique);
+    }
+
+    let mut p = p.clone();
+    let mut x = x.clone();
+    let mut r = r.clone();
+
+    for v in p.clone().iter() {
+        let neighbors = HashSet::from_iter(connections[v].iter().cloned());
+
+        let pcopy = HashSet::from_iter(p.intersection(&neighbors).cloned());
+        let xcopy = HashSet::from_iter(x.intersection(&neighbors).cloned());
+        r.insert(v.clone());
+        bron_kerbosch(connections, &r, &pcopy, &xcopy, maximal_cliques);
+        r.remove(v);
+        p.remove(v);
+        x.insert(v.clone());
+    }
+}
+
+fn largest_interconnected_set(connections: &Connections) -> String {
+    let mut maximal_cliques = HashSet::new();
+    bron_kerbosch(
+        connections,
+        &HashSet::new(),
+        &HashSet::from_iter(connections.keys().cloned()),
+        &HashSet::new(),
+        &mut maximal_cliques,
+    );
+    let max_set_size = maximal_cliques.iter().map(|set| set.len()).max().unwrap();
+    let max_set = maximal_cliques
+        .iter()
+        .find(|&set| set.len() == max_set_size)
+        .unwrap();
+
+    let mut max_vec: Vec<String> = Vec::from_iter(max_set.iter().cloned());
+    max_vec.sort();
+    max_vec.join(",")
+}
+
 fn parse_input(data: &str) -> Connections {
     let lines = data.lines();
     let pairs = lines.map(|line| line.split_once('-').unwrap());
@@ -68,6 +118,10 @@ fn main() {
         "count of interconnected computers with t: {}",
         count_interconnected_computers_with_t(&connections)
     );
+    println!(
+        "largest interconnected set: {}",
+        largest_interconnected_set(&connections)
+    );
 }
 
 #[cfg(test)]
@@ -86,5 +140,22 @@ mod tests {
         let data = read_to_string("src/main.txt").unwrap();
         let connections: HashMap<String, Vec<String>> = parse_input(&data);
         assert_eq!(count_interconnected_computers_with_t(&connections), 1215);
+    }
+
+    #[test]
+    fn test_part2() {
+        let data = read_to_string("src/test.txt").unwrap();
+        let connections: HashMap<String, Vec<String>> = parse_input(&data);
+        assert_eq!(largest_interconnected_set(&connections), "co,de,ka,ta");
+    }
+
+    #[test]
+    fn answer_part2() {
+        let data = read_to_string("src/main.txt").unwrap();
+        let connections: HashMap<String, Vec<String>> = parse_input(&data);
+        assert_eq!(
+            largest_interconnected_set(&connections),
+            "bm,by,dv,ep,ia,ja,jb,ks,lv,ol,oy,uz,yt"
+        );
     }
 }
